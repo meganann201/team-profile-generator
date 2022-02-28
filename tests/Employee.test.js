54974a71b74ff9bdccdf18a85483136db2fe0e75
Megan Campbell <megancampbell201@gmail.com>
2022-02-28 14:12:53 -0500

    installed jest and added node_modules to .gitignore file

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,1 +1,9 @@
 const Employee = require('../lib/Employee.js');
+
+test('creates an employee object', () => {
+    const employee = new Employee('Dave', '123', 'user@email.com');
+  
+    expect(employee.name).toBe('Dave');
+    expect(employee.id).toBe('123');
+    expect(employee.email).toBe('user@email.com');
+});
